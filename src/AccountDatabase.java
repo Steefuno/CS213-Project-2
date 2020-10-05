d7a42477689c2584df491569ad5017a651ad1e0c
Steve <pickleman20@hotmail.com>
2020-10-04 21:40:47 -0400

    Completed documenting and test cases for AccountDatabase.java

--- src/AccountDatabase.java
+++ src/AccountDatabase.java
@@ -1,31 +1,51 @@
 import java.text.DecimalFormat;
 
+/**
+ * This class handles storing and handling accounts
+ * @author Steven Nguyen, Julian Romero
+ */
 public class AccountDatabase {
 	private Account[] accounts;
 	private int size;
 
 	private static final int GROW_SIZE = 5;
 
+	/**
+	 * Constructs a default account database with 0 accounts
+	 */
 	public AccountDatabase() {
 		this.size = 0;
 		this.accounts = new Account[5];
 	}
 
+	/**
+	 * Searches for an account with a given First and Last name
+	 * @param account	an account with the same first and last name as the account to find
+	 * @return			the index of the found account or -1
+	 */
 	private int find(Account account) {
 		for (int i = 0; i < this.size; i++) {
-			if (accounts[i] == account) {
+			if (accounts[i].equals(account)) {
 				return i;
 			}
 		}
 		return -1;
 	}
 	
+	/**
+	 * Raises the pseudo arraylist's size
+	 */
 	private void grow() {
 		Account[] expandedAccounts = new Account[this.accounts.length + GROW_SIZE];
 		System.arraycopy(this.accounts, 0, expandedAccounts, 0, this.accounts.length);
 		this.accounts = expandedAccounts;
 	}
 	
+	/**
+	 * Adds an account to the database
+	 * @param account	the account to add to the database
+	 * @return			true if successfully added, false otherwise
+	 */
 	public boolean add(Account account) {
 		if (this.find(account) != -1) {
 			return false;
@@ -40,6 +60,11 @@ public class AccountDatabase {
 		return true;
 	}
 	
+	/**
+	 * Removes an account from the database
+	 * @param account	an account with the same first and last name as the account to remove
+	 * @return			true if the account was successfully removed, false otherwise
+	 */
 	public boolean remove(Account account) {
 		int accountIndex = this.find(account);
 		if (accountIndex == -1) {
@@ -55,6 +80,12 @@ public class AccountDatabase {
 		return true;
 	}
 	
+	/**
+	 * Deposits money into an account
+	 * @param account	an account with the same first and last name as the account to add to
+	 * @param amount	the amount of money to add to the account
+	 * @return			true if the money is successfully deposited, false otherwise
+	 */
 	public boolean deposit(Account account, double amount) {
 		int accountIndex = this.find(account);
 		if (accountIndex == -1) {
@@ -67,6 +98,13 @@ public class AccountDatabase {
 		return true;
 	}
 	
+	/**
+	 * Withdraws money from an account
+	 * @param account	an account with the same first and last name as the account to withdraw from
+	 * @param amount	the amount of money to withdraw
+	 * @return			-1 if the account is not found, 1 if the account doesn't have enough money to withdraw,<br>
+	 * and 0 if successfully withdrawn
+	 */
 	public int withdrawal(Account account, double amount) {
 		int accountIndex = this.find(account);
 		if (accountIndex == -1) {
@@ -82,6 +120,13 @@ public class AccountDatabase {
 		return 0;
 	}
 
+	/**
+	 * Recursively sorts the accounts by dateOpened<br>
+	 * Uses quicksort with the pivot as the last element<br>
+	 * Sorts by last name if the dates are the same
+	 * @param startIndex	the start index of accounts to start sorting from
+	 * @param endIndex		the index after the last index of accounts to sort from
+	 */
 	/*private void sortByDateOpenHelper(int startIndex, int endIndex) {
 		int pivotIndex = endIndex - 1;
 		
@@ -89,8 +134,10 @@ public class AccountDatabase {
 			return;
 		
 		Date pivot = accounts[pivotIndex].getOpenDate();
+		Profile pivotProfile = accounts[pivotIndex].getProfile();
 		int smallerIndex = startIndex;
 		
+		// For each account, if it is to be ordered before the pivot, swap with the index after the previously ordered account 
 		for (int i = startIndex; i < endIndex; i++) {
 			Account account = accounts[i];
 			Date date = account.getOpenDate();
@@ -100,22 +147,42 @@ public class AccountDatabase {
 				accounts[i] = accounts[smallerIndex];
 				accounts[smallerIndex] = account;
 				smallerIndex++;
+			// Compares the account names if they were opened on the same day
+			} else if (dateComparison == 0) {
+				int nameComparison = account.getProfile().compareTo(pivotProfile);
+				if (nameComparison < 0) {
+					accounts[i] = accounts[smallerIndex];
+					accounts[smallerIndex] = account;
+					smallerIndex++;
+				}
 			}
 		}
 		
+		// Swaps the pivot with the index after the previously ordered account
 		Account account = accounts[smallerIndex];
 		accounts[smallerIndex] = accounts[pivotIndex];
 		accounts[pivotIndex] = account;
 		
+		// Sorts the left and right partition of the pivot
 		sortByLastNameHelper(startIndex, smallerIndex);
 		sortByLastNameHelper(smallerIndex + 1, endIndex);
 		return;
-	}
-	
-	private void sortByDateOpen() {
-		sortByDateOpenHelper(0, size);
 	}*/
 	
+	/**
+	 * Calls sortByDateOpenHelper to recursively sort accounts by date opened
+	 */
+	/*private void sortByDateOpen() {
+		sortByDateOpenHelper(0, size);
+	}*/
+
+	/**
+	 * Recursively sorts the accounts by last name<br>
+	 * Uses quicksort with the pivot as the last element<br>
+	 * Sorts by first name if the last names are the same
+	 * @param startIndex	the start index of accounts to start sorting from
+	 * @param endIndex		the index after the last index of accounts to sort from
+	 */
 	private void sortByLastNameHelper(int startIndex, int endIndex) {
 		int pivotIndex = endIndex - 1;
 		
@@ -124,7 +191,8 @@ public class AccountDatabase {
 		
 		Profile pivot = accounts[pivotIndex].getProfile();
 		int smallerIndex = startIndex;
-		
+
+		// For each account, if it is to be ordered before the pivot, swap with the index after the previously ordered account 
 		for (int i = startIndex; i < endIndex; i++) {
 			Account account = accounts[i];
 			Profile profile = account.getProfile();
@@ -136,20 +204,29 @@ public class AccountDatabase {
 				smallerIndex++;
 			}
 		}
-		
+
+		// Swaps the pivot with the index after the previously ordered account
 		Account account = accounts[smallerIndex];
 		accounts[smallerIndex] = accounts[pivotIndex];
 		accounts[pivotIndex] = account;
-		
+
+		// Sorts the left and right partition of the pivot
 		sortByLastNameHelper(startIndex, smallerIndex);
 		sortByLastNameHelper(smallerIndex + 1, endIndex);
 		return;
 	}
-	
+
+	/**
+	 * Calls sortByLastNameHelper to recursively sort accounts by last name
+	 */
 	private void sortByLastName() {
 		sortByLastNameHelper(0, this.size);
 	}
 	
+	/**
+	 * Updates an account's balance for a new month with interest and the monthly fee
+	 * @param accountIndex	the index of the account to update in accounts
+	 */
 	private void updateAccount(int accountIndex) {
 		Account account = accounts[accountIndex];
 		double interest = account.monthlyInterest();
@@ -175,7 +252,15 @@ public class AccountDatabase {
 		return;
 	}
 	
+	/**
+	 * Sorts accounts by date opened, updates all the accounts, then outputs them
+	 */
 	/*public void printByDateOpen() {
+		if (size == 0) {
+			System.out.println("Database is empty.");
+			return;
+		}
+		
 		sortByDateOpen();
 		
 		System.out.println("--Printing statements by last name--");
@@ -187,8 +272,16 @@ public class AccountDatabase {
 		System.out.println("--end of printing--");
 		return;
 	}*/
-	
+
+	/**
+	 * Sorts accounts by last name, updates all the accounts, then outputs them
+	 */
 	public void printByLastName() {
+		if (size == 0) {
+			System.out.println("Database is empty.");
+			return;
+		}
+		
 		sortByLastName();
 		
 		System.out.println("--Printing statements by last name--");
@@ -200,8 +293,16 @@ public class AccountDatabase {
 		System.out.println("--end of printing--");
 		return;
 	}
-	
+
+	/**
+	 * Outputs all of the accounts without updating
+	 */
 	public void printAccounts() {
+		if (size == 0) {
+			System.out.println("Database is empty.");
+			return;
+		}
+		
 		System.out.println("--Listing accounts in the database--");
 		
 		for (int i = 0; i < this.size; i++) {
@@ -210,5 +311,129 @@ public class AccountDatabase {
 		
 		System.out.println("--end of listing--");
 		return;
+	}
+	
+	/**
+	 * Testmain - tests methods used in AccountDatabase.java
+	 * @param args	unused
+	 */
+	public static void main(String[] args) {
+		AccountDatabase db = new AccountDatabase();
+		
+		// Tests if the prints are correct when the database is empty
+		db.printAccounts();
+		db.printByLastName();
+		//db.printByDateOpen();
+		
+		// Tests adding different types of accounts
+		if (db.add(new Checking("A", "A", 100, 2, 2, 2001, false)) == true) {
+			System.out.println("Expected: added A A Checking");
+		} else {
+			System.out.println("Unexpected: cannot add A A Checking");
+		}
+		
+		if (db.add(new MoneyMarket("A", "Banana", 100, 2, 1, 2001)) == true) {
+			System.out.println("Expected: added A Banana MoneyMarket");
+		} else {
+			System.out.println("Unexpected: cannot add A Banana MoneyMarket");
+		}
+		
+		if (db.add(new Checking("B", "A", 100, 2, 2, 2000, false)) == true) {
+			System.out.println("Expected: added B A Checking");
+		} else {
+			System.out.println("Unexpected: cannot add B A Checking");
+		}
+		
+		if (db.add(new Savings("C", "C", 100, 1, 2, 2001, false)) == true) {
+			System.out.println("Expected: added C C Savings");
+		} else {
+			System.out.println("Unexpected: cannot add C C Savings");
+		}
+		
+		// Tests is adding a duplicate account is invalid
+		if (db.add(new Checking("A", "A", 100, 2, 1, 2001, false)) == true) {
+			System.out.println("Unexpected: added another A A Checking");
+		} else {
+			System.out.println("Expected: cannot add another A A Checking");
+		}
+		
+		// Tests if depositing into an account works
+		if (db.deposit(new Checking("A", "A", 0, 1, 1, 2000, false), 50) == true) {
+			System.out.println("Expected: deposited into A A Checking");
+		} else {
+			System.out.println("Unexpected: cannot deposit into A A Checking");
+		}
+		
+		// Tests if depositing into an invalid account is invalid
+		if (db.deposit(new Checking("Z", "Z", 0, 1, 1, 2000, false), 50) == true) {
+			System.out.println("Unexpected: deposited into account that does not exist");
+		} else {
+			System.out.println("Expected: cannot deposit into account that does not exists");
+		}
+		
+		// Tests if withdrawing from an invalid account is invalid
+		int withdrawResult = db.withdrawal(new Checking("Z", "Z", 0, 1, 1, 2000, false), 100);
+		if (withdrawResult == -1) {
+			System.out.println("Expected: cannot withdraw from account that does not exist");
+		} else {
+			System.out.println("Unexpected: withdraw returned: " + withdrawResult);
+		}
+		
+		// Tests if withdrawing from an account with insufficient balance is invalid
+		withdrawResult = db.withdrawal(new Savings("C", "C", 0, 1, 1, 2000, false), 101);
+		if (withdrawResult == 1) {
+			System.out.println("Expected: account C C Savings does not have enough balance");
+		} else {
+			System.out.println("Unexpected: withdraw returned: " + withdrawResult);
+		}
+		
+		// Tests if withdrawing from a valid account is valid
+		withdrawResult = db.withdrawal(new Savings("C", "C", 0, 1, 1, 2000, false), 100);
+		if (withdrawResult == 0) {
+			System.out.println("Expected: withdrew 100 from account C C Savings");
+		} else {
+			System.out.println("Unexpected: withdraw returned: " + withdrawResult);
+		}
+		
+		// Tests outputting accounts with accounts stored
+		db.printAccounts();
+		db.printByLastName();
+		//db.printByDateOpen();
+		
+		// Tests removing an account with a matching name, but different account type is invalid
+		if (db.remove(new Savings("A", "A", 100, 2, 1, 2001, false)) == true) {
+			System.out.println("Unexpected: removed A A Savings that shouldn't exist");
+		} else {
+			System.out.println("Expected: cannot remove an account that doesn't exist");
+		}
+		
+		// Tests if removing a valid account is valid
+		if (db.remove(new Checking("A", "A", 100, 2, 1, 2001, false)) == true) {
+			System.out.println("Expected: removed A A Checking");
+		} else {
+			System.out.println("Unexpected: cannot remove A A Checking");
+		}
+
+		// Tests if removing a removed account is valid
+		if (db.remove(new Checking("A", "A", 100, 2, 1, 2001, false)) == true) {
+			System.out.println("Unexpected: removed A A Checking that shouldn't exist");
+		} else {
+			System.out.println("Expected: cannot remove an account that doesn't exist");
+		}
+		
+		// Tests if removed accounts are not outputted
+		db.printAccounts();
+		db.printByLastName();
+		//db.printByDateOpen();
+		
+		// Tests if more than 5 accounts can be added
+		for (int i = 1; i < 10; i++) {
+			db.add(new Checking("F" + i, "L" + i, 100, 1, i, 2002, false));
+		}
+		
+		// Tests if more than 5 accounts can be outputted
+		db.printAccounts();
+		db.printByLastName();
+		//db.printByDateOpen();
 	}
 }
