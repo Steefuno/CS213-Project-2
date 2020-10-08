cd5b43627928cc18ec694230db1e30c7577beb90
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-10-08 08:38:17 -0400

    Update RunProject2.java

--- src/account/RunProject2.java
+++ src/account/RunProject2.java
@@ -1,7 +1,14 @@
 package account;
-
+/**
+ * This class runs the project
+ * @author Steven Nguyen, Julian Romero
+ */
 public class RunProject2 {
-
+	
+	/**
+	 * Makes a transactionManager and runs it
+	 * @param args	unused
+	 */
 	public static void main(String[] args) {
 		new TransactionManager().run();
 	}
