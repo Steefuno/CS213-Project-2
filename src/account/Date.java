a58267ca8213f922824bfe2afd0dfae28e194457
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-10-08 06:55:20 -0400

    Update Date.java

--- src/account/Date.java
+++ src/account/Date.java
@@ -2,16 +2,28 @@ package account;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class represents a date following the format mm/dd/yyyy
+ * @author Steven Nguyen, Julian Romero
+ */
 public class Date implements Comparable<Date> {
 	 private int year;
 	 private int month;
 	 private int day;
 	 
+	 /**
+	 * Constructs a date object
+	 * @param _month	the month of the open date
+	 * @param _day		the day of the open date
+	 * @param _year		the year of the open date
+	 */
 	 public Date(int month, int day, int year) {
 		 this.month = month;
 		 this.day = day;
 		 this.year = year;
 	 }
+	 
+	 
 	
 	 public int compareTo(Date date) {
 		 if (date.isValid()) {
@@ -26,6 +38,11 @@ public class Date implements Comparable<Date> {
 			 return -1;
 		 }
 	 } 
+	/**
+	* Gets the string representation of this Date<br>
+	* Format: *Month*, *day* ,*year*
+	* @return	the string representation
+	*/	 
 	 
 	 public String toString() {
 		 String output = String.format("%d/%d/%d", this.month, this.day, this.year);
@@ -34,11 +51,12 @@ public class Date implements Comparable<Date> {
 		 
 	  //in the format mm/dd/yyyy
 	public boolean isValid() {
-		Pattern pattern = Pattern.compile("^((0|1|2)\\d{1})\\/(([0-3])\\d{1})\\/((17|18|19|20)\\d{2})");  
+		
+		Pattern pattern = Pattern.compile("^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$");  
 		Matcher matcher = pattern.matcher(this.toString());
 
 	
-		    if(matcher.find()){ //string has right format
+		    if(matcher.matches()){ 
 		       return true;
 		    }
 		    else {
